3c965b582c509b507c3e8b3a89e4f13b239ce77e
Magalon <antoine@hakouna.com>
2018-07-29 20:36:34 -0400

    Update Text

--- src/Pages/Projects/index.js
+++ src/Pages/Projects/index.js
@@ -26,9 +26,8 @@ class Projects extends React.Component {
         },
         {
             image: '../images/TronWatch_DEX.png',
-            title: 'Decentralized Exchange',
-            subTitle: 'Dolor Sit',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+            title: 'TronWatch DEX',
+            subTitle: 'Decentralized Exchange',
         }
     ]
 
